ef253d8841e9c77810a32cee56c6210422b55ba1
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2022-11-15 10:10:11 -0300

    Update iteration.js

--- fibonacci/iteration.js
+++ fibonacci/iteration.js
@@ -2,7 +2,7 @@
 const fibS = (value) => {
     const numbers = [0]
     let number = 1
-    for (i = 0; i < value-1; i++) {
+    for (i = 0; i < value; i++) {
         numbers.push(number)
         number += numbers[i]
     }
